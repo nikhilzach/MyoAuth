7dba9995b19814942e4a100a55acc3867b8c85a3
NikhilZachariah <nikhilzach5@gmail.com>
2024-01-03 18:30:00 +0000

    Committing lines with date Thu Jan 04 2024 00:00:00 GMT+0530 (India Standard Time)

---
+++ temp_main9.py
@@ -0,0 +1,12 @@
+#main8.py de 2 second version
+
+#main5 cont and separate login and register
+
+#introducing a new title window and audioo
+
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+import csv
+import serial
+import threading
+from datetime import datetime
