1f883f4e2277235a5b350218ca2862a21e9fce2b
NikhilZachariah <nikhilzach5@gmail.com>
2024-01-08 18:30:00 +0000

    Committing lines with date Tue Jan 09 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-#main8.py de 2 second version
-
-#main5 cont and separate login and register
-
-#introducing a new title window and audioo
-
-import tkinter as tk
-from tkinter import ttk, filedialog, messagebox
-import csv
-import serial
-import threading
-from datetime import datetime
+import time
+import numpy as np
+from scipy.stats import skew, kurtosis
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import os
+import joblib
+from PIL import Image, ImageTk  # Import Image and ImageTk from PIL
+import pygame  # Import pygame library
+import imageio
