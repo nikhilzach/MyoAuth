ef2934111974bbcf16a6893a660a58c5f1f304a3
NikhilZachariah <nikhilzach5@gmail.com>
2024-01-22 18:30:00 +0000

    Committing lines with date Tue Jan 23 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-from ttkthemes import ThemedStyle
-from tkinter import font
+        style = ThemedStyle(self.root)
+        style.set_theme("plastik") 
 
+        self.gif_label = None
+        self.step = None
+        self.user_name = None
+        self.initial_option = None
 
-class EMGRecorderApp:
-    def __init__(self, root, serial_port):
-        self.root = root
-        self.root.title("MyoAuth")
-        self.root.geometry("700x500")  # Set window size
-        self.root.iconbitmap(default='myoauthicon.ico')
-        self.root.configure(bg='white')
-
+        self.knn_classifier = None
+        self.scaler = None
+
+        # Load and display the image
