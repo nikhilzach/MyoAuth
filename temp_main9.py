8069cf283f06b3feeac749cd66721ebfdc755534
NikhilZachariah <nikhilzach5@gmail.com>
2024-01-26 18:30:00 +0000

    Committing lines with date Sat Jan 27 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-        style = ThemedStyle(self.root)
-        style.set_theme("plastik") 
+        image_path = 'myoauthicon.png'  # Replace with your image path
+        original_image = Image.open(image_path)
+        resized_image = original_image.resize((200, 200), Image.LANCZOS)
+        self.image = ImageTk.PhotoImage(resized_image)
+        self.image_label = tk.Label(self.root, image=self.image,background="white")
+        self.image_label.place(relx=0.5, rely=0.4, anchor='center')
 
-        self.gif_label = None
-        self.step = None
-        self.user_name = None
-        self.initial_option = None
+        # Create a label for the title
+        self.title_label = ttk.Label(self.root, text="MyoAuth", font=("Calibri", 25), foreground='green',background="white")
+        self.title_label.place(relx=0.5, rely=0.65, anchor='center')
 
-        self.knn_classifier = None
-        self.scaler = None
-
-        # Load and display the image
+        # Play audio when the title is shown
