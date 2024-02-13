5d031f7c2b5241d170543ce2ed46a10196a68751
NikhilZachariah <nikhilzach5@gmail.com>
2024-02-13 18:30:00 +0000

    Committing lines with date Wed Feb 14 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-            current_volume -= step
-            pygame.mixer.music.set_volume(max(0.1, current_volume))
-            pygame.time.delay(int(step))
+        style = ttk.Style()
+        style.configure("TButton", padding=6, relief="flat", background="#ccc")
 
-    def show_main_window(self):
-        # Destroy the title label
-        self.title_label.destroy()
-        self.image_label.destroy()
+        welcome_label = tk.Label(self.root, text="Welcome to MyoAuth", background="white", font=("Helvetica", 15, "bold"), fg="green")
+        welcome_label.place(relx=0.5, rely=0.1, anchor='center')
 
-        self.clear_window()
+        existing_user_label = tk.Label(self.root, text="Existing User?",background="white", font=("Helvetica", 10))
+        existing_user_label.place(relx=0.5, rely=0.3, anchor='center')
 
-        # Create buttons for the main window
+        # Create "Login" and "Register" buttons
+        self.login_button = ttk.Button(self.root, text="Login", command=self.show_login_window)
+        self.login_button.place(relx=0.5, rely=0.37, anchor='center', width=150, height=40)
