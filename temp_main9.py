54ef5dc0414f72b37ff8325a8f8bbc469d83656c
NikhilZachariah <nikhilzach5@gmail.com>
2024-02-19 18:30:00 +0000

    Committing lines with date Tue Feb 20 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-        style = ttk.Style()
-        style.configure("TButton", padding=6, relief="flat", background="#ccc")
 
-        welcome_label = tk.Label(self.root, text="Welcome to MyoAuth", background="white", font=("Helvetica", 15, "bold"), fg="green")
-        welcome_label.place(relx=0.5, rely=0.1, anchor='center')
+        new_user_label = tk.Label(self.root, text="New User?",background="white", font=("Helvetica", 10))
+        new_user_label.place(relx=0.5, rely=0.6, anchor='center')
 
-        existing_user_label = tk.Label(self.root, text="Existing User?",background="white", font=("Helvetica", 10))
-        existing_user_label.place(relx=0.5, rely=0.3, anchor='center')
+        self.register_button = ttk.Button(self.root, text="Register", command=self.show_register_window)
+        self.register_button.place(relx=0.5, rely=0.67, anchor='center', width=150, height=40)
 
-        # Create "Login" and "Register" buttons
-        self.login_button = ttk.Button(self.root, text="Login", command=self.show_login_window)
-        self.login_button.place(relx=0.5, rely=0.37, anchor='center', width=150, height=40)
+
+        self.serial_port = serial_port
+        self.emg_data = {}
+        self.data_thread = None
+        self.load_classifier()
