472a650b65bc99056b3048899ff3d52e7c432c1c
NikhilZachariah <nikhilzach5@gmail.com>
2024-02-23 18:30:00 +0000

    Committing lines with date Sat Feb 24 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
 
-        new_user_label = tk.Label(self.root, text="New User?",background="white", font=("Helvetica", 10))
-        new_user_label.place(relx=0.5, rely=0.6, anchor='center')
+    def show_login_window(self):
+        # Clear the window
+        self.clear_window()
 
-        self.register_button = ttk.Button(self.root, text="Register", command=self.show_register_window)
-        self.register_button.place(relx=0.5, rely=0.67, anchor='center', width=150, height=40)
+        self.initial_option = "login"
 
+        login_label = tk.Label(self.root, text="User Login", background="white", font=("Helvetica", 15, "bold"), fg="green")
+        login_label.place(relx=0.5, rely=0.1, anchor='center')
 
-        self.serial_port = serial_port
-        self.emg_data = {}
-        self.data_thread = None
-        self.load_classifier()
+        self.user_label = ttk.Label(self.root, text="Enter Username", background="white", font=("Helvetica", 12))
+        self.user_label.place(relx=0.5, rely=0.35, anchor='center')
