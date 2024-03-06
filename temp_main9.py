44a810a72f9254eae49b0b6fd4e7b574ac180772
NikhilZachariah <nikhilzach5@gmail.com>
2024-03-06 18:30:00 +0000

    Committing lines with date Thu Mar 07 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
+        self.clear_window()
 
-        self.user_entry = ttk.Entry(self.root, justify="center", font=("Helvetica", 12))
-        self.user_entry.place(relx=0.5, rely=0.42, anchor='center', width=200)
+        self.initial_option = "register"
 
-        self.verify_button = ttk.Button(self.root, text="Verify", command=self.verify_person)
-        self.verify_button.place(relx=0.5, rely=0.55, anchor='center', width=150, height=35)
-
-        self.back_button = ttk.Button(self.root, text="Back", command=self.show_main_window)
-        self.back_button.place(relx=0.5, rely=0.65, anchor='center', width=150, height=35)
-
-    def show_register_window(self):
-        # Clear the window
+        register_label = tk.Label(self.root, text="New User Registration", background="white", font=("Helvetica", 15, "bold"), fg="green")
+        register_label.place(relx=0.5, rely=0.1, anchor='center')
+        
+        self.step = 0
+        
+        # Create "Record EMG Signals," "Extract Features," and "Train" buttons
+        self.record_button = ttk.Button(self.root, text="Record EMG Signals", command=self.start_recording_instructions)
+        self.record_button.place(relx=0.5, rely=0.3, anchor='center', width=200, height=40)
