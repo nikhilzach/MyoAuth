c786696eaf4d95eacb2ced701d000a3788f372bd
NikhilZachariah <nikhilzach5@gmail.com>
2024-03-18 18:30:00 +0000

    Committing lines with date Tue Mar 19 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
+        # This function will be called when the "Next" button is clicked
+        self.step = self.step + 1
+        if(self.step<=5):
+            self.start_recording()
+        else:
+            self.show_register_window()
 
-        self.preprocess_button = ttk.Button(self.root, text="Extract Features", command=self.feature_extract_data)
-        self.preprocess_button.place(relx=0.5, rely=0.4, anchor='center', width=200, height=40)
-
-        self.train_button = ttk.Button(self.root, text="Train", command=self.train_classifier)
-        self.train_button.place(relx=0.5, rely=0.5, anchor='center', width=150, height=35)
-
-        self.back_button = ttk.Button(self.root, text="Back", command=self.show_main_window)
-        self.back_button.place(relx=0.5, rely=0.6, anchor='center', width=150, height=35)
-
-    def step_counter(self):
-        # Add your logic for the step counter here
+    def start_recording_instructions(self):
+        
+        self.clear_window()
+        self.step=0
+
