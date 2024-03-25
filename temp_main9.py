83e0b2291a4d905e22bc3010da21d816f25f6145
NikhilZachariah <nikhilzach5@gmail.com>
2024-03-25 18:30:00 +0000

    Committing lines with date Tue Mar 26 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-        # This function will be called when the "Next" button is clicked
-        self.step = self.step + 1
-        if(self.step<=5):
-            self.start_recording()
-        else:
-            self.show_register_window()
-
-    def start_recording_instructions(self):
+        # Create instructions label
+        self.instructions_label = ttk.Label(self.root, text="Follow these instructions", font=("Helvetica", 14,"bold"), background="white",foreground="green")
+        self.instructions_label.place(relx=0.5, rely=0.1, anchor='center')
         
-        self.clear_window()
-        self.step=0
-
+
+        self.instructions_label2 = ttk.Label(self.root, text="You will be guided through a series of five steps.", font=("Helvetica", 12), background="white")
+        self.instructions_label2.place(relx=0.5, rely=0.2, anchor='center')
+
+        self.instructions_label3 = ttk.Label(self.root, text="During each step, execute a hand gesture for a duration of 2 seconds.", font=("Helvetica", 12), background="white")
+        self.instructions_label3.place(relx=0.5, rely=0.3, anchor='center')
+
+        self.instructions_label4 = ttk.Label(self.root, text="It is necessary to maintain uniformity by consistently performing the same gesture at each step.", font=("Helvetica", 12), background="white")
