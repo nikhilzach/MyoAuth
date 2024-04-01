642eea2624696c77dd58e321acf5baceb641889d
NikhilZachariah <nikhilzach5@gmail.com>
2024-04-01 18:30:00 +0000

    Committing lines with date Tue Apr 02 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-        # Create instructions label
-        self.instructions_label = ttk.Label(self.root, text="Follow these instructions", font=("Helvetica", 14,"bold"), background="white",foreground="green")
-        self.instructions_label.place(relx=0.5, rely=0.1, anchor='center')
-        
+        self.instructions_label4.place(relx=0.5, rely=0.35, anchor='center')
 
-        self.instructions_label2 = ttk.Label(self.root, text="You will be guided through a series of five steps.", font=("Helvetica", 12), background="white")
-        self.instructions_label2.place(relx=0.5, rely=0.2, anchor='center')
+        self.user_label = ttk.Label(self.root, text="Before we start, enter Username", font=("Helvetica", 11), background="white")
+        self.user_label.place(relx=0.5, rely=0.45, anchor='center')
 
-        self.instructions_label3 = ttk.Label(self.root, text="During each step, execute a hand gesture for a duration of 2 seconds.", font=("Helvetica", 12), background="white")
-        self.instructions_label3.place(relx=0.5, rely=0.3, anchor='center')
+        self.user_entry = ttk.Entry(self.root, justify="center", font=("Helvetica", 12),width=30)
+        self.user_entry.place(relx=0.5, rely=0.52, anchor='center')
 
-        self.instructions_label4 = ttk.Label(self.root, text="It is necessary to maintain uniformity by consistently performing the same gesture at each step.", font=("Helvetica", 12), background="white")
+        # Create "Start Recording" button
+        self.next_button = ttk.Button(self.root, text="Start Recording", command=self.step_counter)
+        self.next_button.place(relx=0.5, rely=0.6, anchor='center')
+
