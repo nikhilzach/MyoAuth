0a10b459cec8800f368a49684b13e665d1788f09
NikhilZachariah <nikhilzach5@gmail.com>
2024-04-13 18:30:00 +0000

    Committing lines with date Sun Apr 14 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-        self.back_button = ttk.Button(self.root, text="Back", command=self.show_register_window)
-        self.back_button.place(relx=0.5, rely=0.7, anchor='center')
-
-    def clear_window(self):
-        # Destroy all widgets in the window
-        for widget in self.root.winfo_children():
-            widget.destroy()
-
-    def start_recording(self):
-        if(self.step==1):
-            user_name = self.user_entry.get()
-            self.user_name = user_name
+            if(user_name!=""):
+                user_name=user_name+str(self.step)
+        else:
+            user_name=self.user_name+str(self.step)
+        
+        print(user_name)
+            
+        if not user_name:
+            tk.messagebox.showinfo("Error", "Please enter Username.")
+            self.step=0
+            return
+
