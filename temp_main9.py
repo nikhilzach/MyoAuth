1777ae54de87683b65f37b658f1686edc23db937
NikhilZachariah <nikhilzach5@gmail.com>
2024-04-20 18:30:00 +0000

    Committing lines with date Sun Apr 21 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-            if(user_name!=""):
-                user_name=user_name+str(self.step)
-        else:
-            user_name=self.user_name+str(self.step)
-        
-        print(user_name)
-            
-        if not user_name:
-            tk.messagebox.showinfo("Error", "Please enter Username.")
-            self.step=0
-            return
-
+        if user_name not in self.emg_data:
+            self.emg_data[user_name] = []  # Create an empty list for the user if not present
+
+        self.emg_data.clear()
+        self.emg_data[user_name] = []
+        self.clear_window()
+
+        self.data_thread = threading.Thread(target=self.record_emg, args=(user_name,))
+        self.data_thread.start()
+
+        step_label = tk.Label(self.root, text="", font=("Helvetica", 15,"bold"), background="white",foreground="green")
+        step_label.place(relx=0.5, rely=0.1, anchor='center')
