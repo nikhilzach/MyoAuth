3226cb5a1f97c5d36f1e2dc26cf38fe8ae4f658a
NikhilZachariah <nikhilzach5@gmail.com>
2024-05-20 18:30:00 +0000

    Committing lines with date Tue May 21 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-    def animate_gif(self, index, duration):
-        # Display the current frame of the animated GIF
-        self.gif_label.config(image=self.gif_photos[index])
 
-        # Schedule the next frame to be shown after a delay
-        if index < len(self.gif_photos) - 1:
-            self.root.after(int(duration * 1000 / len(self.gif_photos)), self.animate_gif, index + 1, duration)
-        elif self.initial_option=="register":
-            # Schedule the static image to be shown after the animated GIF
-            self.root.after(int(duration * 160), self.show_static_image, 'tick2.png')
-        else:
-            pass
+    def show_static_image(self, image_path):
+        static_image = Image.open(image_path)
+        resized_image = static_image.resize((100, 100), Image.LANCZOS)
+        static_photo = ImageTk.PhotoImage(resized_image)
+
+        # Replace the GIF label with a static image label
+        self.gif_label.config(image=static_photo)
+        self.gif_label.image = static_photo
+
+        # Play audio
+        pygame.init()
