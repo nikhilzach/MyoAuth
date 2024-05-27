5924cf754ad95c200b8441feb14ac2e6f3a9dde0
NikhilZachariah <nikhilzach5@gmail.com>
2024-05-27 18:30:00 +0000

    Committing lines with date Tue May 28 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
+        pygame.mixer.music.load('myoauthintro.mp3')  # Replace with your audio file path
+        pygame.mixer.music.play(0,0,1)  # Start playing
+        
+        # Create "Next" button
+        if(self.step<5):
+            self.next_button = ttk.Button(self.root, text="Next Step", command=self.step_counter)
+            self.next_button.place(relx=0.5, rely=0.6, anchor='center')
+        else:
+            self.next_button = ttk.Button(self.root, text="Finish", command=self.step_counter)
+            self.next_button.place(relx=0.5, rely=0.6, anchor='center')
 
-    def show_static_image(self, image_path):
-        static_image = Image.open(image_path)
-        resized_image = static_image.resize((100, 100), Image.LANCZOS)
-        static_photo = ImageTk.PhotoImage(resized_image)
-
-        # Replace the GIF label with a static image label
-        self.gif_label.config(image=static_photo)
-        self.gif_label.image = static_photo
-
-        # Play audio
-        pygame.init()
+
