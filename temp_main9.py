554921a1d222de4d86096254885371b99945cbee
NikhilZachariah <nikhilzach5@gmail.com>
2024-05-31 18:30:00 +0000

    Committing lines with date Sat Jun 01 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-        pygame.mixer.music.load('myoauthintro.mp3')  # Replace with your audio file path
-        pygame.mixer.music.play(0,0,1)  # Start playing
-        
-        # Create "Next" button
-        if(self.step<5):
-            self.next_button = ttk.Button(self.root, text="Next Step", command=self.step_counter)
-            self.next_button.place(relx=0.5, rely=0.6, anchor='center')
-        else:
-            self.next_button = ttk.Button(self.root, text="Finish", command=self.step_counter)
-            self.next_button.place(relx=0.5, rely=0.6, anchor='center')
+    def record_emg(self, user_name):
+        print(user_name)
+        with serial.Serial(self.serial_port, 115200, timeout=1) as ser:
+            ser.reset_input_buffer()
 
-
+            start_time = time.time()
+            while time.time() - start_time < 2:
+                try:
+                    line = ser.readline().decode().strip()
+                    if line:
+                        timestamp = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+                        emg_value = float(line)
