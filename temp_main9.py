60cc3e8c7f4be3f2ad4a4ac94fba9f71406659d9
NikhilZachariah <nikhilzach5@gmail.com>
2024-06-10 18:30:00 +0000

    Committing lines with date Tue Jun 11 2024 00:00:00 GMT+0530 (India Standard Time)

--- temp_main9.py
+++ temp_main9.py
@@ -1,12 +1,12 @@
-                        self.emg_data[user_name].append((timestamp, emg_value))
-                except ValueError as e:
-                    print(f"Error parsing data: {e}")
 
-        # Save data to CSV after recording
-        self.save_to_csv()
+        with open(filename, 'a', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
 
-    def save_to_csv(self):
-        filename = "all_users_emg_data.csv"
+            # Write header only if the file doesn't exist
+            if not file_exists:
+                csv_writer.writerow(["Username", "timestamps", "emgvalues"])
 
-        # Check if the file already exists
-        file_exists = os.path.isfile(filename)
+            for user_name, data in self.emg_data.items():
+                print(user_name)
+                for timestamp, emg_value in data:
+                    csv_writer.writerow([user_name, timestamp, emg_value])
